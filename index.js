674192442797fa9b7004054450eb57bd009f2112
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2023-12-03 20:57:48 +0200

    added user credentials sent to db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-b667d-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://couple-todos-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
@@ -16,58 +16,61 @@ const shoppingListEl = document.getElementById("shopping-list")
 // =========================================== Experimental
 // =========================================== Experimental
 
+// user credentials to database START
 // DB = database
 // LS = localstorage
 
-localStorage.setItem("isUserLogged", JSON.stringify("false"))
-const isUserLoggedLS = (JSON.parse(localStorage.getItem("isUserLogged")))
+const usersInDB = ref(database, "usersCredentials")
+
+const loginButtonEl = document.getElementById("login-button")
+const singInButtonEl = document.getElementById("singin-button")
+
 let usernameFieldEl = document.getElementById("username-field")
 let passwordFieldEl = document.getElementById("password-field")
-const loginButtonEl = document.getElementById("login-button")
+
+let usernameValue = ""
+let passwordValue = ""
 
 
-// if (isUserLoggedLS === "false") {
-//     let loginContainerEl = document.getElementById("login-container")
-//     loginContainerEl.innerHTML = `
-//     <div>
-//         <img src="assets/cloud.png">
-//         <input type="text" id="username-field" placeholder="Username...">
-//         <input type="text" id="password-field" placeholder="Username...">
-//         <button id="login-button">Login</button>
-//     </div>
-//     `
-//     if (!userLogin()) {
-//         alert("true user login")
-//         loginContainerEl.innerHTML = ""
-//         loginContainerEl.style.float
-//         loginContainerEl.zIndex = "-1"
-//     }
-// }
-
-// loginButtonEl.addEventListener("click", function() {
-//     userLogin(usernameFieldEl.value, passwordFieldEl.value)
-// })
+loginButtonEl.addEventListener("click", addUserInDB)
+function addUserInDB() {
+    usernameValue = usernameFieldEl.value
+    passwordValue = passwordFieldEl.value
+    push(usersInDB, [usernameValue, passwordValue])
+    
+    let exactUserLocationInDB = ""
+    // exactUserLocationInDB = ref(database, `usersCredentials/${usernameValue, passwordValue}`)
+    
+    console.log(exactUserLocationInDB)
+    if (exactUserLocationInDB) {
+        console.log("The user already exists")
+    }
+    
+    
+    // remove(exactUserLocationInDB)
+}
 
 
-// function userLogin(userName, userPassword) {
-//     console.log("sending data to firebase database")
-//     let isUserInDatabase = checkUserCredentials(userName, userPassword)
-//     if (isUserInDatabase) {
-//         console.log("IF is user in database, returned true")
-//         return true
-//     } else {
-//         console.log("IF user isn't in database, returned false")
-//         return false
-//     }
-// }
+onValue(usersInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let usersArray = Object.entries(snapshot.val())
+        // clearShoppingListEl()
+        
+        for (let i = 0; i < usersArray.length; i++) {
+            let currentUser = usersArray[i]
+            let currentUserID = currentUser[0]
+            let currentUserValue = currentUser[1]     
+            console.log(currentUserID)
+            console.log(currentUserValue)
+            // appendToShoppingListEl(currentItem)
+        }    
+    } else {
+        console.log("idk... else")
+    }
+})
+// user credentials to database END
 
-
-// function checkUserCredentials(x, z) {
-//     // send data to database and check if it's a match
-//     // if it is, return true else return false
-//     return true
-// }
-
+// Login pop-up window START
 const openLoginWindow = document.getElementById("open-login-window-btn")
 const loginWindow = document.getElementById("login-window")
 const loginButton = document.getElementById("login-button")
@@ -82,15 +85,12 @@ openLoginWindow.addEventListener("click", function(event) {
 loginButton.addEventListener("click", function() {
     loginWindow.style.display = "none"
 })
-
-
-// -------------------------------------------------------- section
-
+// Login pop-up window END
 
 
 
 
-
+// -------------------------------------------------------- ready section
 
 // Change the background color on mobile devices
 function changeBGColorOnMobile(changeElBG) {
@@ -101,16 +101,17 @@ function changeBGColorOnMobile(changeElBG) {
     }
 }
 
-
 // Change the height of category baset on elements in it
-$(":root").css("--category-multiply", "4")
+document.documentElement.style.setProperty("--category-multiply", "4")
+// $(":root").css("--category-multiply", "4")
+
+
 // =========================================== Experimental
 // =========================================== Experimental
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
-    
     changeBGColorOnMobile(addButtonEl)
     clearInputFieldEl()
 })
@@ -118,18 +119,16 @@ addButtonEl.addEventListener("click", function() {
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-    
         clearShoppingListEl()
         
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            appendItemToShoppingListEl(currentItem)
+            // let currentItemID = currentItem[0]
+            // let currentItemValue = currentItem[1]     
+            appendToShoppingListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        shoppingListEl.innerHTML = "No items... yet"
     }
 })
 
@@ -141,21 +140,15 @@ function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(item) {
+function appendToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
-    
     let newEl = document.createElement("li")
-    
     newEl.textContent = itemValue
     
     newEl.addEventListener("click", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
-        remove(exactLocationOfItemInDB)
+        let exactItemLocationInDB = ref(database, `shoppingList/${itemID}`) 
+        remove(exactItemLocationInDB)
     })
-    
     shoppingListEl.append(newEl)
 }
-
-// =============================

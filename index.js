dc600acbefff64b0acae7b35e40aad60bf2d2011
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2023-12-03 13:18:43 +0200

    added "on click" bg color change in JS

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ const shoppingListEl = document.getElementById("shopping-list")
 // DB = database
 // LS = localstorage
 
-
-
-
 localStorage.setItem("isUserLogged", JSON.stringify("false"))
 const isUserLoggedLS = (JSON.parse(localStorage.getItem("isUserLogged")))
 let usernameFieldEl = document.getElementById("username-field")
@@ -29,61 +26,92 @@ let passwordFieldEl = document.getElementById("password-field")
 const loginButtonEl = document.getElementById("login-button")
 
 
-if (isUserLoggedLS === "false") {
-    let loginContainerEl = document.getElementById("login-container")
-    loginContainerEl.innerHTML = `
-    <div>
-        <img src="assets/cloud.png">
-        <input type="text" id="username-field" placeholder="Username...">
-        <input type="text" id="password-field" placeholder="Username...">
-        <button id="login-button">Login</button>
-    </div>
-    `
-    if (!userLogin()) {
-        alert("true user login")
-        loginContainerEl.innerHTML = ""
-        loginContainerEl.style.float
-        loginContainerEl.zIndex = "-1"
-    }
-}
+// if (isUserLoggedLS === "false") {
+//     let loginContainerEl = document.getElementById("login-container")
+//     loginContainerEl.innerHTML = `
+//     <div>
+//         <img src="assets/cloud.png">
+//         <input type="text" id="username-field" placeholder="Username...">
+//         <input type="text" id="password-field" placeholder="Username...">
+//         <button id="login-button">Login</button>
+//     </div>
+//     `
+//     if (!userLogin()) {
+//         alert("true user login")
+//         loginContainerEl.innerHTML = ""
+//         loginContainerEl.style.float
+//         loginContainerEl.zIndex = "-1"
+//     }
+// }
 
-loginButtonEl.addEventListener("click", function() {
-    userLogin(usernameFieldEl.value, passwordFieldEl.value)
+// loginButtonEl.addEventListener("click", function() {
+//     userLogin(usernameFieldEl.value, passwordFieldEl.value)
+// })
+
+
+// function userLogin(userName, userPassword) {
+//     console.log("sending data to firebase database")
+//     let isUserInDatabase = checkUserCredentials(userName, userPassword)
+//     if (isUserInDatabase) {
+//         console.log("IF is user in database, returned true")
+//         return true
+//     } else {
+//         console.log("IF user isn't in database, returned false")
+//         return false
+//     }
+// }
+
+
+// function checkUserCredentials(x, z) {
+//     // send data to database and check if it's a match
+//     // if it is, return true else return false
+//     return true
+// }
+
+const openLoginWindow = document.getElementById("open-login-window-btn")
+const loginWindow = document.getElementById("login-window")
+const loginButton = document.getElementById("login-button")
+
+// Show the pop-up window when the link is clicked
+openLoginWindow.addEventListener("click", function(event) {
+    event.preventDefault()
+    loginWindow.style.display = "block"
+})
+
+// Hide the pop-up window when the close button is clicked
+loginButton.addEventListener("click", function() {
+    loginWindow.style.display = "none"
 })
 
 
-function userLogin(userName, userPassword) {
-    console.log("sending data to firebase database")
-    let isUserInDatabase = checkUserCredentials(userName, userPassword)
-    if (isUserInDatabase) {
-        console.log("IF is user in database, returned true")
-        return true
-    } else {
-        console.log("IF user isn't in database, returned false")
-        return false
+// -------------------------------------------------------- section
+
+
+
+
+
+
+
+// Change the background color on mobile devices
+function changeBGColorOnMobile(changeElBG) {
+    changeElBG.style.backgroundColor = "#232D3F"  // green | --main-bg-color
+    setTimeout(changeBack, 200, changeElBG)
+    function changeBack(changeElBG) {
+        changeElBG.style.backgroundColor = "#005B41"  // dark blue | --main-bg-hover-color
     }
 }
 
 
-function checkUserCredentials(x, z) {
-    // send data to database and check if it's a match
-    // if it is, return true else return false
-    return true
-}
-
-
-
-
-
-
+// Change the height of category baset on elements in it
+$(":root").css("--category-multiply", "4")
 // =========================================== Experimental
 // =========================================== Experimental
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
     push(shoppingListInDB, inputValue)
     
+    changeBGColorOnMobile(addButtonEl)
     clearInputFieldEl()
 })
 

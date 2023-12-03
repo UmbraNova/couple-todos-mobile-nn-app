91d492c0548b74c6d0180386a8be6e45383c2f3c
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2023-12-03 08:34:53 +0200

    first step to saving user login

--- index.js
+++ index.js
@@ -13,6 +13,72 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+// =========================================== Experimental
+// =========================================== Experimental
+
+// DB = database
+// LS = localstorage
+
+
+
+
+localStorage.setItem("isUserLogged", JSON.stringify("false"))
+const isUserLoggedLS = (JSON.parse(localStorage.getItem("isUserLogged")))
+let usernameFieldEl = document.getElementById("username-field")
+let passwordFieldEl = document.getElementById("password-field")
+const loginButtonEl = document.getElementById("login-button")
+
+
+if (isUserLoggedLS === "false") {
+    let loginContainerEl = document.getElementById("login-container")
+    loginContainerEl.innerHTML = `
+    <div>
+        <img src="assets/cloud.png">
+        <input type="text" id="username-field" placeholder="Username...">
+        <input type="text" id="password-field" placeholder="Username...">
+        <button id="login-button">Login</button>
+    </div>
+    `
+    if (!userLogin()) {
+        alert("true user login")
+        loginContainerEl.innerHTML = ""
+        loginContainerEl.style.float
+        loginContainerEl.zIndex = "-1"
+    }
+}
+
+loginButtonEl.addEventListener("click", function() {
+    userLogin(usernameFieldEl.value, passwordFieldEl.value)
+})
+
+
+function userLogin(userName, userPassword) {
+    console.log("sending data to firebase database")
+    let isUserInDatabase = checkUserCredentials(userName, userPassword)
+    if (isUserInDatabase) {
+        console.log("IF is user in database, returned true")
+        return true
+    } else {
+        console.log("IF user isn't in database, returned false")
+        return false
+    }
+}
+
+
+function checkUserCredentials(x, z) {
+    // send data to database and check if it's a match
+    // if it is, return true else return false
+    return true
+}
+
+
+
+
+
+
+// =========================================== Experimental
+// =========================================== Experimental
+
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     

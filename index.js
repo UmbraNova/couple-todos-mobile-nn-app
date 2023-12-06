faae8150caea21af7a8640fcb84db891a6a118d2
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2023-12-06 22:38:25 +0200

    deleted default group name LS

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ const loginWindow = document.getElementById("login-window")
 
 let loginErrorInfoEl = document.getElementById("login-error-info-el")
 
-console.log(JSON.parse(localStorage.getItem("groupNameLS")))
-if (JSON.parse(localStorage.getItem("groupNameLS")) == null) {
-    console.log("mamaia")
-    localStorage.setItem("groupNameLS", JSON.stringify("vakidavra"))
-}
-console.log(JSON.parse(localStorage.getItem("groupNameLS")))
+// console.log(JSON.parse(localStorage.getItem("groupNameLS")))
+// if (JSON.parse(localStorage.getItem("groupNameLS")) == null) {
+//     console.log("mamaia")
+//     localStorage.setItem("groupNameLS", JSON.stringify("vakidavra"))
+// }
+// console.log(JSON.parse(localStorage.getItem("groupNameLS")))
+
 
 let isUserLoggedInGroup = JSON.parse( localStorage.getItem("isUserLogged"))
 let groupNameLS = JSON.parse(localStorage.getItem("groupNameLS"))
@@ -162,7 +163,6 @@ document.documentElement.style.setProperty("--category-multiply", "4")
 // })
 
 addButtonEl.addEventListener("click", function() {
-    addButtonEl.style.color = "red"
     if (isUserLoggedInGroup == true) {
         let inputValue = inputFieldEl.value
         push(itemsListInDB, inputValue)
